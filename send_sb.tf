d97e277001262db397eb3961d2356a3b2f4a07f2
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2021-11-13 19:32:27 +0530

    Fix json data.

--- send_sb.tf
+++ send_sb.tf
@@ -12,6 +12,7 @@ provider "restapi" {
 resource "restapi_object" "sa_credentials" {
   path = "/clusters/da93036a-f43a-4dce-8dc8-55aff17d1201/ingress-info"
   data = jsonencode({
+      "id": "da93036a-f43a-4dce-8dc8-55aff17d1201",
       "foo" : "bar",
       "baz": "${var.cluster_name}"
       })
